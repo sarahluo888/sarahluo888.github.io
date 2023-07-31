c7b0f0fc5724e99802f69661deb10f8747ead510
Sarah Luo <sarrstar888@gmail.com>
2023-07-30 21:40:17 -0700

    pages

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home/home';
+// import Timeline from './pages/Timeline/timeline';
+import Footer from './components/Footer/footer';
+import Projects from './pages/Projects/projects';
+import Background from './pages/Background/background';
+// import Photography from './pages/Photography/photography';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Router>
+          {/* <NavBar /> */}
+          <div className="body">
+            <Routes>
+              <Route exact path="/" component={Home}/>
+              {/* <Route path="/about" component={About}/> */}
+              <Route path="/background" component={Background}/>
+              {/* <Route path="/timeline" component={Timeline}/> */}
+              <Route path="/projects" component={Projects}/>
+              {/* <Route path="/gallery" component={Photography}/> */}
+            </Routes>
+            <Footer />
+          </div>
+
+        </Router>
     </div>
   );
 }
